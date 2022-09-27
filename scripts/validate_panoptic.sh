cc289d0455e51f09897f5bad2dc67b09a63116d9
kyky233 <danqi.allison.yan@gmail.com>
2022-09-27 18:39:28 +0800

    /

--- scripts/validate_panoptic.sh
+++ scripts/validate_panoptic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -J VOXELPOSE_MVHW_TRAIN
+#SBATCH -J VOXELPOSE_MVHW_VAL
 #SBATCH -p p-V100
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH -o /mntnfs/med_data4/wangjiong/3dpose_school/voxelpose_dev/slurm_logs/train_%j.out
 #SBATCH -e /mntnfs/med_data4/wangjiong/3dpose_school/voxelpose_dev/slurm_logs/train_%j.out
 #SBATCH --mail-type=ALL  # BEGIN,END,FAIL,ALL
@@ -13,9 +13,9 @@
 
 # export MASTER_PORT=$((12000 + $RANDOM % 2000))
 set -x
-CONFIG=configs/mvhw/train_prn64_cpn80x80x20.yaml
+CONFIG=configs/mvhw/prn64_cpn80x80x20.yaml
 
 # PYTHONPATH="$(dirname ./scripts/train_panoptic.sh)/..":$PYTHONPATH \
 which python
 
-python -m torch.distributed.launch --nproc_per_node=2 --use_env run/train_3d.py --cfg $CONFIG
+python -m torch.distributed.launch --nproc_per_node=1 --use_env run/validate_3d.py --cfg $CONFIG
