52d3b45c91b0ad6ce25222d9e0fe9933f795b3f7
kyky233 <danqi.allison.yan@gmail.com>
2022-10-09 18:13:49 +0800

    /

--- scripts/train_mvhw_single.sh
+++ scripts/train_mvhw_single.sh
@@ -13,8 +13,8 @@
 
 # export MASTER_PORT=$((12000 + $RANDOM % 2000))
 set -x
-#CONFIG=configs/mvhw/train_prn64_cpn80x80x20_single.yaml
-CONFIG=configs/mvhw/train_prn64_512x960_cpn80x80x20_single.yaml
+#CONFIG=configs/mvhw/train_960x512_single.yaml
+CONFIG=configs/mvhw/train_512x960_single.yaml
 
 # PYTHONPATH="$(dirname ./scripts/train_mvhw.sh)/..":$PYTHONPATH \
 which python
