d84bd2e33a33aa5533dd10325c054e9048971d58
kyky233 <danqi.allison.yan@gmail.com>
2022-10-13 05:39:42 +0800

    /

--- lib/models/higher_hrnet.py
+++ lib/models/higher_hrnet.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import torch
 from torch import nn
 # from models.modules import BasicBlock, Bottleneck
