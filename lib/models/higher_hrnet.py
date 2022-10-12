5f8ba94a37f3ffdf1ecb41289b35693a90c6cbf7
kyky233 <danqi.allison.yan@gmail.com>
2022-10-13 05:47:58 +0800

    /

--- lib/models/higher_hrnet.py
+++ lib/models/higher_hrnet.py
@@ -324,7 +324,7 @@ def get_pose_net(cfg, is_train, **kwargs):
     model = HigherHRNet(c=32, nof_joints=17, bn_momentum=0.1)
 
     # load weight
-    model.load_state_dict(torch.load(cfg.NETWORK.PRETRAINED_BACKBONE))
+    model.load_state_dict(torch.load(cfg.NETWORK.PRETRAINED_BACKBONE)['state_dict'])
 
     return model
 
