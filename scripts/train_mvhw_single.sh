529c3bcd3d89e2d1890acb7055bfb11abe56d23f
kyky233 <danqi.allison.yan@gmail.com>
2022-10-14 17:01:55 +0800

    /

--- scripts/train_mvhw_single.sh
+++ scripts/train_mvhw_single.sh
@@ -17,10 +17,10 @@ set -x
 #CONFIG=configs/mvhw/train_512x960_single.yaml
 #CONFIG=configs/mvhw/train_512x960_single_lr_same.yaml
 #CONFIG=configs/mvhw/train_512x960_single_lr_different.yaml
-#CONFIG=configs/mvhw/train_512x960_single_synthetic.yaml
+CONFIG=configs/mvhw/train_512x960_single_synthetic.yaml
 #CONFIG=configs/mvhw/train_512x960_single_hrnet.yaml
 #CONFIG=configs/mvhw/train_512x960_single_coco.yaml
-CONFIG=configs/mvhw/train_512x960_single_coco_diff_lr.yaml
+#CONFIG=configs/mvhw/train_512x960_single_coco_diff_lr.yaml
 #CONFIG=configs/mvhw/train_512x960_single_use_gt.yaml
 
 
